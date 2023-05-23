c9c8b3fb7c609bb32da93956bcc349d5a06f0750
Samuel Reeder <samuel.reeder8@gmail.com>
2023-05-22 21:45:08 -0600

    fix

--- src/nicegpt/text.py
+++
@@ -1,9 +0,0 @@
-import nicegpt
-
-nicegpt.config("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")
-
-gpt_instance = nicegpt.GPT(model=nicegpt.Model.GPT4)  
-
-response = gpt_instance.response("What is the capital of France?")
-
-print(response)  
